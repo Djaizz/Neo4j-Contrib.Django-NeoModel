062000a9e9a1edfbce427543886a9d94664e6a10
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2026-01-08 14:02:05 -0800

    refactor NeoManager.get_queryset()

--- django_neomodel/__init__.py
+++ django_neomodel/__init__.py
@@ -176,7 +176,7 @@ class NeoManager:
         self.model = model
 
     def get_queryset(self):
-        return NodeSet(self.model)
+        return NodeSet(source=self.model)
 
 
 class MetaClass(NodeMeta):
